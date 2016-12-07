180e2addefe0f93771075eaabecc6d086dba2674
ALPHA-YOGA910\Alpha <dial.alpha@gmail.com>
2016-12-07 10:37:42 -0500

    finish step 5 - services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,18 @@
-import { Component } from '@angular/core';
-import { HeroDetailComponent } from './hero-detail.component';
+import { Component, OnInit } from '@angular/core';
 
-export class Hero{
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+export class Hero {
   id: number;
   name: string
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 
 @Component({
+  providers: [HeroService],
   selector: 'my-app',
-  template:`
+  template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -34,7 +24,7 @@ const HEROES: Hero[] = [
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
-    styles: [`
+  styles: [`
       .selected {
         background-color: #CFD8DC !important;
         color: white;
@@ -85,11 +75,27 @@ const HEROES: Hero[] = [
     `]
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  onSelect(hero: Hero): void{
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+     this.heroService.getHeroes()
+      .then(result => this.heroes = result);
+  }
+
+  /// OnInit interface - lifecylce hook
+  ngOnInit(): void{
+    this.getHeroes();
   }
 }
