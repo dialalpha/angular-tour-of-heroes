9a82be4666dd9960476cf6b244e4ddc37baf637d
ALPHA-YOGA910\Alpha <dial.alpha@gmail.com>
2016-12-07 21:21:09 -0500

    finish step 6 - routing

---
+++ app/heroes.component.ts
@@ -0,0 +1,50 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
+export class Hero {
+  id: number;
+  name: string
+}
+
+
+@Component({
+  moduleId: module.id,
+  providers: [HeroService],
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html' ,
+  styleUrls: [
+    'heroes.component.css'
+    ]
+
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+     this.heroService.getHeroes()
+      .then(result => this.heroes = result);
+  }
+
+  gotoDetail = () : void => {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  /// OnInit interface - lifecylce hook
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+}
